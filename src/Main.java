9807bbea5f1dd0cd4387dbb4f10fc3c7685a06e7
Achmelev96 <achmelev96@gmail.com>
2025-03-08 14:44:21 +0100

    Complite

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
 
 public class Main extends Application {
 
